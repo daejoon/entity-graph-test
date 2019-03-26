7d8dbb8be6d83109922b7929edfcee405d38be79
daejoon <kkode1911@gmail.com>
2019-03-26 13:49:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# EntityGraph Test
+> @EntityGraph를 사용해서 Lazy, Eager를 테스트 해본다.
+
+## 실행방법
+```
+$ ./gradlew bootRun
+```
+
+## 참조
+- [Spring Boot 2.1.3.RELEASE](https://docs.spring.io/spring-boot/docs/2.1.3.RELEASE/reference/htmlsingle/)
---
+++ src/main/java/com/ddoong2/entitygraphtest/EntityGraphTestApplication.java
@@ -0,0 +1,13 @@
+package com.ddoong2.entitygraphtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityGraphTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityGraphTestApplication.class, args);
+    }
+
+}
